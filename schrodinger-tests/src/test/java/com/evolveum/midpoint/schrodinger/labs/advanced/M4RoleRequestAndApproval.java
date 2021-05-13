4ea96945ea00a656068a4f7c68247ade60a26c4e
kate <honchar@evolveum.com>
2021-05-14 01:06:42 +0200

    started with lab 1 from module 4

--- schrodinger-tests/src/test/java/com/evolveum/midpoint/schrodinger/labs/advanced/M4RoleRequestAndApproval.java
+++ schrodinger-tests/src/test/java/com/evolveum/midpoint/schrodinger/labs/advanced/M4RoleRequestAndApproval.java
@@ -2,6 +2,7 @@ package com.evolveum.midpoint.schrodinger.labs.advanced;
 
 import org.apache.commons.io.FileUtils;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 import java.io.File;
 import java.io.IOException;
@@ -15,6 +16,13 @@ public class M4RoleRequestAndApproval extends AbstractAdvancedLabTest {
     private static final File CSV_2_SOURCE_FILE = new File(M3_LAB_SOURCES_DIRECTORY + "csv-2.csv");
     private static final File CSV_3_SOURCE_FILE = new File(M3_LAB_SOURCES_DIRECTORY + "csv-3.csv");
     private static final File CONTRACTORS_SOURCE_FILE = new File(M3_LAB_SOURCES_DIRECTORY + "contractors.csv");
+    private static final File SYSTEM_CONFIGURATION_FILE_4_1 = new File(LAB_OBJECTS_DIRECTORY + "systemconfiguration/system-configuration-4-1.xml");
+    private static final File SYSTEM_CONFIGURATION_FILE_4_2 = new File(LAB_OBJECTS_DIRECTORY + "systemconfiguration/system-configuration-4-2.xml");
+    private static final File ROLE_META_POLICY_RULE_BIGBROTHER = new File(LAB_OBJECTS_DIRECTORY + "roles/role-meta-policy-rule-bigbrother.xml");
+    private static final File ROLE_META_POLICY_RULE_APPROVER = new File(LAB_OBJECTS_DIRECTORY + "roles/role-meta-policy-rule-approver.xml");
+    private static final File ROLE_META_POLICY_RULE_SECURITY_OFFICER = new File(LAB_OBJECTS_DIRECTORY + "roles/role-meta-policy-rule-security-officer-skip-employees.xml");
+    private static final File ROLE_META_POLICY_RULE_USER_MANAGER = new File(LAB_OBJECTS_DIRECTORY + "roles/role-meta-policy-rule-user-manager.xml");
+    private static final File ROLE_ORG_EXAMPLE_APPROVER_POLICY_ROOT = new File(LAB_OBJECTS_DIRECTORY + "orgs/org-example-approver-policy-root.xml");
 
     @BeforeClass(alwaysRun = true, dependsOnMethods = { "springTestContextPrepareTestInstance" })
     @Override
@@ -37,5 +45,50 @@ public class M4RoleRequestAndApproval extends AbstractAdvancedLabTest {
         FileUtils.copyFile(CONTRACTORS_SOURCE_FILE, contractorsTargetFile);
     }
 
+    @Test(groups={"advancedM1"})
+    public void mod04test01configureApprovalsUsingPolicyRules() throws IOException {
+        addObjectFromFile(SYSTEM_CONFIGURATION_FILE_4_1);
+        addObjectFromFile(ROLE_META_POLICY_RULE_BIGBROTHER);
+        addObjectFromFile(ROLE_META_POLICY_RULE_APPROVER);
+        addObjectFromFile(ROLE_META_POLICY_RULE_SECURITY_OFFICER);
+        addObjectFromFile(ROLE_META_POLICY_RULE_USER_MANAGER);
+
+        basicPage
+                .orgStructure()
+                    .selectTabWithRootOrg("ExAmPLE, Inc. Approval Policies")
+                        .getOrgHierarchyPanel()
+                            .selectOrgInTree("ExAmPLE, Inc. Approval Policies")
+                            .and()
+                        .getMemberPanel()
+                            .assignMember()
+                                .selectType("Role")
+                                .table()
+                                    .selectCheckboxByName("Metarole - Request Additional Approval by Big Brother")
+                                    .selectCheckboxByName("Metarole - Request Approval by Role Approver(s)")
+                                    .selectCheckboxByName("Metarole - Request Approval by Security Officer for Non-employees")
+                                    .selectCheckboxByName("Metarole - Request Approval by User Manager(s)")
+                                    .and()
+                                .clickAdd()
+                            .getMemberPanel()
+                                .selectType("All")
+                                .table()
+                                    .assertTableContainsText("Metarole - Request Additional Approval by Big Brother")
+                                    .assertTableContainsText("Metarole - Request Approval by Role Approver(s)")
+                                    .assertTableContainsText("Metarole - Request Approval by Security Officer for Non-employees")
+                                    .assertTableContainsText("Metarole - Request Approval by User Manager(s)");
+
+        addObjectFromFile(SYSTEM_CONFIGURATION_FILE_4_2);
+
+        showUser("administrator")
+                .selectTabBasic()
+                    .form()
+                        .showEmptyAttributes("Properties")
+                        .addAttributeValue("Email", "administrator@example.com")
+                        .and()
+                    .and()
+                .clickSave()
+                    .feedback()
+                        .assertSuccess();
+    }
 
 }
