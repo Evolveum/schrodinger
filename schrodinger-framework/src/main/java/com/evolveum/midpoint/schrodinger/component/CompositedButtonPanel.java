306baa0d8f855fa84aa9fca0e8972f5fd6ad708a
kate <honchar@evolveum.com>
2021-08-17 16:58:53 +0200

    multi composited button panel coverage

---
+++ schrodinger-framework/src/main/java/com/evolveum/midpoint/schrodinger/component/CompositedButtonPanel.java
@@ -0,0 +1,55 @@
+package com.evolveum.midpoint.schrodinger.component;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import com.evolveum.midpoint.schrodinger.MidPoint;
+
+public class CompositedButtonPanel<T> extends Component<T> {
+
+    public CompositedButtonPanel(T parent, SelenideElement parentElement) {
+        super(parent, parentElement);
+    }
+
+    public String getBasicIconCss() {
+        return getParentElement().$x(".//i[@data-s-id='basicIcon']")
+                .waitUntil(Condition.visible, MidPoint.TIMEOUT_SHORT_4_S).getAttribute("class");
+    }
+
+    public String getTopRightLayerIconCss() {
+        return getParentElement().$x(".//i[contains(@class,\"top-right-layer\")]")
+                .waitUntil(Condition.visible, MidPoint.TIMEOUT_SHORT_4_S).getAttribute("class");
+    }
+
+    public String getBottomRightLayerIconCss() {
+        return getParentElement().$x(".//i[contains(@class,\"bottom-right-layer\")]")
+                .waitUntil(Condition.visible, MidPoint.TIMEOUT_SHORT_4_S).getAttribute("class");
+    }
+
+    public String getLabelValue() {
+        return getParentElement().$x(".//span[@class=\"compositedButtonLabel\")]")
+                .waitUntil(Condition.visible, MidPoint.TIMEOUT_SHORT_4_S).getValue();
+    }
+
+    public void assertBasicIconCssEquals(String expectedValue) {
+        assertion.assertEquals(getBasicIconCss(), expectedValue, "Basic icon class doesn't equal, ");
+    }
+
+    public void assertBasicIconCssContains(String expectedValue) {
+        assertion.assertTrue(getBasicIconCss() != null && getBasicIconCss().contains(expectedValue),
+                "Basic icon class doesn't contain value " + expectedValue + ", ");
+    }
+
+    public void assertBottomRightLayerIconCssContains(String expectedValue) {
+        assertion.assertTrue(getBottomRightLayerIconCss() != null && getBottomRightLayerIconCss().contains(expectedValue),
+                "Bottom right icon class doesn't contain value " + expectedValue + ", ");
+    }
+
+    public void assertTopRightLayerIconCssContains(String expectedValue) {
+        assertion.assertTrue(getTopRightLayerIconCss() != null && getTopRightLayerIconCss().contains(expectedValue),
+                "Top right icon class doesn't contain value " + expectedValue + ", ");
+    }
+
+    public void assertLabelEquals(String expectedValue) {
+        assertion.assertEquals(getBasicIconCss(), expectedValue, "Label doesn't equal, ");
+    }
+}
---
+++ schrodinger-framework/src/main/java/com/evolveum/midpoint/schrodinger/component/MultiCompositedButtonPanel.java
@@ -0,0 +1,34 @@
+package com.evolveum.midpoint.schrodinger.component;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+import com.evolveum.midpoint.schrodinger.MidPoint;
+import com.evolveum.midpoint.schrodinger.util.Schrodinger;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$x;
+
+public class MultiCompositedButtonPanel<T> extends Component<T> {
+
+    public MultiCompositedButtonPanel(T parent, SelenideElement parentElement) {
+        super(parent, parentElement);
+    }
+
+    public CompositedButtonPanel<MultiCompositedButtonPanel<T>> findCompositedButtonByTitle(String buttonTitle) {
+        SelenideElement button = $(Schrodinger.byElementAttributeValue("button", "data-original-title", buttonTitle))
+                .waitUntil(Condition.visible, MidPoint.TIMEOUT_DEFAULT_2_S);
+        return new CompositedButtonPanel<>(this, button);
+    }
+
+    public CompositedButtonPanel<MultiCompositedButtonPanel<T>> findCompositedButtonByLabel(String buttonLabel) {
+        SelenideElement button = $(Schrodinger.byElementValue("span", "class", "compositedButtonLabel", buttonLabel))
+                .waitUntil(Condition.visible, MidPoint.TIMEOUT_DEFAULT_2_S);
+        return new CompositedButtonPanel<>(this, button);
+    }
+
+    public ElementsCollection findCompositedButtonsListByBasicIcon(String basicIcon) {
+        return $$x(".//i[contains(@class,\"" + basicIcon + "\")]");
+    }
+
+}
