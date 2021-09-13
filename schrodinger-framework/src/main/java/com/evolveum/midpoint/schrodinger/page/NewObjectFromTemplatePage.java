327f7db412b9dce29d9dd9d1bb2aea4ba9a2208f
kate <honchar@evolveum.com>
2021-09-13 08:39:15 +0200

    create new object fix

--- schrodinger-framework/src/main/java/com/evolveum/midpoint/schrodinger/page/NewObjectFromTemplatePage.java
+++ schrodinger-framework/src/main/java/com/evolveum/midpoint/schrodinger/page/NewObjectFromTemplatePage.java
@@ -14,7 +14,7 @@ public class NewObjectFromTemplatePage<T extends AssignmentHolderDetailsPage> ex
         SelenideElement templateButton = $(Schrodinger.byElementValue("span",
                 "class", "compositedButtonLabel", title)).waitUntil(Condition.visible, MidPoint.TIMEOUT_MEDIUM_6_S);
         templateButton.click();
-        $(Schrodinger.byDataId("tabPanel")).waitUntil(Condition.visible, MidPoint.TIMEOUT_LONG_1_M);
+        $(Schrodinger.byDataId("mainPanel")).waitUntil(Condition.visible, MidPoint.TIMEOUT_LONG_1_M);
         return pageToRedirect;
     }
 }
