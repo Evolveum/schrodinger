0bb1458c032b1e3a4bce8bb3d1359246f891b817
kate <honchar@evolveum.com>
2021-09-21 09:01:18 +0200

    create from template page fix

--- schrodinger-framework/src/main/java/com/evolveum/midpoint/schrodinger/page/NewObjectFromTemplatePage.java
+++ schrodinger-framework/src/main/java/com/evolveum/midpoint/schrodinger/page/NewObjectFromTemplatePage.java
@@ -1,6 +1,7 @@
 package com.evolveum.midpoint.schrodinger.page;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import com.evolveum.midpoint.schrodinger.MidPoint;
 import com.evolveum.midpoint.schrodinger.util.Schrodinger;
@@ -14,6 +15,7 @@ public class NewObjectFromTemplatePage<T extends AssignmentHolderDetailsPage> ex
         SelenideElement templateButton = $(Schrodinger.byElementValue("span",
                 "class", "compositedButtonLabel", title)).waitUntil(Condition.visible, MidPoint.TIMEOUT_MEDIUM_6_S);
         templateButton.click();
+        Selenide.sleep(MidPoint.TIMEOUT_DEFAULT_2_S);
         $(Schrodinger.byDataId("mainPanel")).waitUntil(Condition.visible, MidPoint.TIMEOUT_LONG_1_M);
         return pageToRedirect;
     }
