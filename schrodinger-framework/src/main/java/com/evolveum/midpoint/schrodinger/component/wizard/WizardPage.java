211315359e0c2a4ba93b7965082a9321ce2f41e8
honchar <honchar@evolveum.com>
2023-01-10 12:42:11 +0100

    started with schrodinger-framework coverage for request access wizard

---
+++ schrodinger-framework/src/main/java/com/evolveum/midpoint/schrodinger/component/wizard/WizardPage.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2023 Evolveum
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.evolveum.midpoint.schrodinger.component.wizard;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import com.evolveum.midpoint.schrodinger.MidPoint;
+import com.evolveum.midpoint.schrodinger.page.BasicPage;
+import com.evolveum.midpoint.schrodinger.util.Schrodinger;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$x;
+
+public class WizardPage extends BasicPage {
+
+    public static final String ID_WIZARD = "wizard";
+
+    public SelenideElement getWizardPanelElement() {
+        return $(Schrodinger.byDataId(ID_WIZARD)).shouldBe(Condition.visible, MidPoint.TIMEOUT_MEDIUM_6_S);
+    }
+
+    public WizardPage assertSelectedStepByLabel(String label) {
+        $x(".//div[@data-s-id='step' and contains(@class, 'active') and " +
+                "descendant-or-self::*[contains(.,\" + label + \")]]").shouldBe(Condition.visible, MidPoint.TIMEOUT_MEDIUM_6_S);
+        return this;
+    }
+
+}
