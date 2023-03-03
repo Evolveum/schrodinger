db3642a104a697a0d490b8210fde66be5a915f35
honchar <honchar@evolveum.com>
2023-03-03 15:49:30 +0100

    one more attempt: tile selection on the template panel fix

--- schrodinger-framework/src/main/java/com/evolveum/midpoint/schrodinger/page/NewObjectFromTemplatePage.java
+++ schrodinger-framework/src/main/java/com/evolveum/midpoint/schrodinger/page/NewObjectFromTemplatePage.java
@@ -8,13 +8,14 @@ import com.evolveum.midpoint.schrodinger.util.Schrodinger;
 import com.evolveum.midpoint.schrodinger.util.Utils;
 
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$x;
 
 public class NewObjectFromTemplatePage<T extends AssignmentHolderDetailsPage> extends BasicPage {
 
 
     public T clickTemplateButtonWithTitle(String title, T pageToRedirect) {
-        SelenideElement templateButton = $(Schrodinger.byElementValue("span",
-                "class", "text-capitalize", title)).shouldBe(Condition.visible, MidPoint.TIMEOUT_MEDIUM_6_S);
+        SelenideElement templateButton = $x(".//span[contains(@class, \"text-capitalize\") " +
+                "and contains(text(), \"" + title + "\")]").shouldBe(Condition.visible, MidPoint.TIMEOUT_MEDIUM_6_S);
         templateButton.click();
         Utils.waitForMainPanelOnDetailsPage();
         Selenide.sleep(MidPoint.TIMEOUT_MEDIUM_6_S.getSeconds());
