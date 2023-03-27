2a46972f378ec6a058304839b40562760f193f83
honchar <honchar@evolveum.com>
2023-03-27 22:38:45 +0200

    change locale before login fix

--- schrodinger-framework/src/main/java/com/evolveum/midpoint/schrodinger/page/BasicPage.java
+++ schrodinger-framework/src/main/java/com/evolveum/midpoint/schrodinger/page/BasicPage.java
@@ -703,7 +703,7 @@ public class BasicPage {
         Utils.waitForAjaxCallFinish();
         SelenideElement localesMenu = $(Schrodinger.byDataId("localesMenu")).shouldBe(Condition.visible, MidPoint.TIMEOUT_MEDIUM_6_S);
         String flagIconCss = "fi-" + countryCode.trim().toLowerCase();
-        localesMenu.$x(".//span[@data-s-id='localesIcon' and contains(@class, '" + flagIconCss + "')]")
+        localesMenu.$x(".//i[@data-s-id='localesIcon' and contains(@class, '" + flagIconCss + "')]")
                 .shouldBe(Condition.visible, MidPoint.TIMEOUT_MEDIUM_6_S)
                 .click();
         Selenide.sleep(2000);
