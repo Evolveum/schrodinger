5e65f7a15a4232bac4a5fc5106926b5de08f9f9e
Kateryna Honchar <gonchar.kate@gmail.com>
2023-06-13 14:55:43 +0200

    resource wizard: more coverage for discovery ans schema steps

--- schrodinger-framework/src/main/java/com/evolveum/midpoint/schrodinger/page/resource/wizard/DiscoveryWizardStep.java
+++ schrodinger-framework/src/main/java/com/evolveum/midpoint/schrodinger/page/resource/wizard/DiscoveryWizardStep.java
@@ -15,14 +15,40 @@
  */
 package com.evolveum.midpoint.schrodinger.page.resource.wizard;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import com.evolveum.midpoint.schrodinger.MidPoint;
+import com.evolveum.midpoint.schrodinger.component.common.PrismForm;
 import com.evolveum.midpoint.schrodinger.component.wizard.NextStepAction;
 import com.evolveum.midpoint.schrodinger.component.wizard.PreviousStepAction;
 import com.evolveum.midpoint.schrodinger.component.wizard.WizardStepPanel;
+
+import static com.codeborne.selenide.Selenide.$x;
 
 public class DiscoveryWizardStep extends WizardStepPanel<ResourceWizardPage>
         implements NextStepAction<SchemaWizardStep>, PreviousStepAction<ConfigurationWizardStep> {
     public DiscoveryWizardStep(ResourceWizardPage parent) {
         super(parent);
+    }
+
+    public DiscoveryWizardStep fieldDelimiter(String value) {
+        getFormPanel().addAttributeValue("Field delimiter", value);
+        return this;
+    }
+
+    public DiscoveryWizardStep userPasswordAttributeName(String value) {
+        getFormPanel().addAttributeValue("User password attribute name", value);
+        return this;
+    }
+
+    public DiscoveryWizardStep nameAttribute (String value) {
+        getFormPanel().addAttributeValue("Name attribute", value);
+        return this;
+    }
+
+    public DiscoveryWizardStep uniqueAttributeName (String value) {
+        getFormPanel().addAttributeValue("Unique attribute name", value);
+        return this;
     }
 
     @Override
@@ -33,6 +59,12 @@ public class DiscoveryWizardStep extends WizardStepPanel<ResourceWizardPage>
     @Override
     public ConfigurationWizardStep back() {
         return new ConfigurationWizardStep(getParent());
+    }
+
+    private PrismForm getFormPanel() {
+        SelenideElement formElement = $x(".//div[@data-s-id='formContainer']")
+                .shouldBe(Condition.visible, MidPoint.TIMEOUT_DEFAULT_2_S);
+        return new PrismForm(DiscoveryWizardStep.this, formElement);
     }
 
 }
--- schrodinger-framework/src/main/java/com/evolveum/midpoint/schrodinger/page/resource/wizard/SchemaWizardStep.java
+++ schrodinger-framework/src/main/java/com/evolveum/midpoint/schrodinger/page/resource/wizard/SchemaWizardStep.java
@@ -1,7 +1,16 @@
 package com.evolveum.midpoint.schrodinger.page.resource.wizard;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import com.evolveum.midpoint.schrodinger.MidPoint;
+import com.evolveum.midpoint.schrodinger.component.common.table.Table;
 import com.evolveum.midpoint.schrodinger.component.wizard.PreviousStepAction;
 import com.evolveum.midpoint.schrodinger.component.wizard.WizardStepPanel;
+import com.evolveum.midpoint.schrodinger.util.Schrodinger;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$x;
 
 public class SchemaWizardStep extends WizardStepPanel<ResourceWizardPage>
         implements PreviousStepAction<DiscoveryWizardStep> {
@@ -9,6 +18,17 @@ public class SchemaWizardStep extends WizardStepPanel<ResourceWizardPage>
         super(parent);
     }
 
+    public SchemaWizardStep selectObjectClassByName(String name) {
+        $(Schrodinger.byFollowingSiblingEnclosedValue("td", "class", "check", "data-s-id", "2", name))
+                .$(By.tagName("input"))
+                .shouldBe(Condition.appear, MidPoint.TIMEOUT_DEFAULT_2_S).click();
+        return this;
+    }
+    private Table getSchemaTableComponent() {
+        SelenideElement table = $x(".//div[@data-s-id='table']")
+                .shouldBe(Condition.visible, MidPoint.TIMEOUT_DEFAULT_2_S);
+        return new Table(SchemaWizardStep.this, table);
+    }
     @Override
     public DiscoveryWizardStep back() {
         return new DiscoveryWizardStep(getParent());
