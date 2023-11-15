0744541692649cd81d30cba7826ca41fa0dba1a2
Kateryna Honchar <honchar@evolveum.com>
2023-11-15 23:05:29 +0100

    finished Module 3 - Lab 3-1 for first steps

--- schrodinger-tests/src/test/java/com/evolveum/midpoint/schrodinger/trainings/first/steps/ImportSourceData.java
+++ schrodinger-tests/src/test/java/com/evolveum/midpoint/schrodinger/trainings/first/steps/ImportSourceData.java
@@ -21,6 +21,24 @@ import org.testng.annotations.Test;
 
 public class ImportSourceData extends AbstractTrainingTest {
 
+    private static final String STATUS_EXPRESSION_SCRIPT_VALUE = "switch (input) {\n" +
+            "    case 'In':\n" +
+            "        'active'\n" +
+            "        break\n" +
+            "\n" +
+            "    case 'Long-term leave':\n" +
+            "        'suspended'\n" +
+            "        break\n" +
+            "\n" +
+            "    case 'Former employee':\n" +
+            "        'archived'\n" +
+            "        break\n" +
+            "\n" +
+            "    //default:\n" +
+            "       //'suspended'\n" +
+            "       //break\n" +
+            "}\n";
+
     @Test(groups = MODULE_3_GROUP)
     public void singleSourceSystemEntryImportSimulation() {
         basicPage
@@ -61,7 +79,57 @@ public class ImportSourceData extends AbstractTrainingTest {
                 .and()
                 .back();
 
+        basicPage
+                .listResources()
+                .table()
+                .clickByName("HR")
+                .selectAccountsPanel()
+                .configureMappings()
+                .inboundMappings()
+                .addInbound()
+                .name("locality-to-locality")
+                .fromResourceAttribute("locality")
+                .expression("As is")
+                .target("locality")
+                .lifecycleState("Active")
+                .and()
+                .addInbound()
+                .name("status-to-lifecycle-state")
+                .fromResourceAttribute("status")
+                .scriptExpression("Groovy (default)", STATUS_EXPRESSION_SCRIPT_VALUE)
+                .target("lifecycleState")
+                .lifecycleState("Active")
+                .and()
+                .and()
+                .saveMappings()
+                .table()
+                .search()
+                .byName()
+                .inputValue("1001")
+                .updateSearch()
+                .and()
+                .importPreview("Name", "1001")
+                .selectTaskExecutionMode("Simulated development")
+                .select()
+                .table()
+                .clickByName("1001")
+                .changes()
+                .header()
+                .assertChangeTypeEquals("Add")
+                .assertChangedObjectTypeEquals("User")
+                .assertChangedObjectNameEquals("1001")
+                .and()
+                .assertItemsDeltasSizeEquals(5) //todo
+                .assertItemValueEquals("Locality", "Small Red Rock City")
+                .assertItemValueEquals("Lifecycle state", "active")
+                .and()
+                .back();
 
+        basicPage
+                .listResources()
+                .table()
+                .clickByName("HR")
+                .setLifecycleState("Active (production)");
     }
 
     @Override
