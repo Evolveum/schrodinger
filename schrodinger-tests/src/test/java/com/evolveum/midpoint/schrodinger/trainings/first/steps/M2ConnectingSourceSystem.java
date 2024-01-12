2aad7cde645f071ff80e6fb0101191b31abaf1bd
Kateryna Honchar <honchar@evolveum.com>
2024-01-12 11:17:14 +0100

    local tests running

--- schrodinger-tests/src/test/java/com/evolveum/midpoint/schrodinger/trainings/first/steps/M2ConnectingSourceSystem.java
+++ schrodinger-tests/src/test/java/com/evolveum/midpoint/schrodinger/trainings/first/steps/M2ConnectingSourceSystem.java
@@ -28,18 +28,19 @@ import java.util.Collections;
 
 public class M2ConnectingSourceSystem extends AbstractTrainingTest {
 
-    @BeforeClass(alwaysRun = true, dependsOnMethods = {"springTestContextPrepareTestInstance"})
-    @Override
-    public void beforeClass() throws IOException {
-        super.beforeClass();
-        hrCsvFile = new File(getTestTargetDir(), CSV_HR_FILE_SOURCE_NAME);
-        FileUtils.copyFile(HR_CSV_SOURCE_FILE, hrCsvFile);
-    }
+//    @BeforeClass(alwaysRun = true, dependsOnMethods = {"springTestContextPrepareTestInstance"})
+//    @Override
+//    public void beforeClass() throws IOException {
+//        super.beforeClass();
+//        hrCsvFile = new File(getTestTargetDir(), CSV_HR_FILE_SOURCE_NAME);
+//        FileUtils.copyFile(HR_CSV_SOURCE_FILE, hrCsvFile);
+//    }
 
 
     @Test(groups = MODULE_2_GROUP)
     public void test1createHrResource() {
-        String csvFilePath = hrCsvFile.getAbsolutePath();
+        //todo should the test do user's export in HR application?
+//        String csvFilePath = hrCsvFile.getAbsolutePath();
         basicPage
                 .newResource()
                 .fromScratch()
@@ -47,7 +48,7 @@ public class M2ConnectingSourceSystem extends AbstractTrainingTest {
                 .name("HR")
                 .lifecycle("Proposed")
                 .next()
-                .filePath(csvFilePath)
+                .filePath("/opt/midpoint/var/resources/export.csv")
                 .next()
                 .uniqueAttributeName("empnum")
                 .next()
