fd28175ffa15d0232cca42129633dc4586369cf8
skublik <lukas.skublik@gmail.com>
2025-01-07 17:00:11 +0100

    adding tests for honeypot on registration page

--- schrodinger-framework/src/main/java/com/evolveum/midpoint/schrodinger/page/login/SelfRegistrationPage.java
+++ schrodinger-framework/src/main/java/com/evolveum/midpoint/schrodinger/page/login/SelfRegistrationPage.java
@@ -15,14 +15,16 @@
  */
 package com.evolveum.midpoint.schrodinger.page.login;
 
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.SelenideElement;
-import com.codeborne.selenide.WebDriverRunner;
+import com.codeborne.selenide.*;
+import com.codeborne.selenide.ex.ElementShould;
 import com.evolveum.midpoint.schrodinger.MidPoint;
 import com.evolveum.midpoint.schrodinger.util.Schrodinger;
 import com.evolveum.midpoint.schrodinger.util.Utils;
+import org.apache.commons.lang3.StringUtils;
 import org.openqa.selenium.JavascriptExecutor;
+
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 
 import static com.codeborne.selenide.Selenide.*;
 
@@ -33,17 +35,40 @@ public class SelfRegistrationPage extends LoginPage {
 
     public SelfRegistrationPage setGivenName(String value) {
         setAttributeValue("firstName", value);
-        return  this;
+        return this;
+    }
+
+    public SelfRegistrationPage setAllGivenNames(String value) {
+        setAllAttributeValue("firstName", value);
+        return this;
     }
 
     public SelfRegistrationPage setFamilyName(String value) {
         setAttributeValue("lastName", value);
-        return  this;
+        return this;
+    }
+
+    public SelfRegistrationPage setAllFamilyNames(String value) {
+        setAllAttributeValue("lastName", value);
+        return this;
     }
 
     public SelfRegistrationPage setEmail(String value) {
         setAttributeValue("email", value);
-        return  this;
+        return this;
+    }
+
+    public SelfRegistrationPage setAllEmails(String value) {
+        setAllAttributeValue("email", value);
+        return this;
+    }
+
+    private void setAllAttributeValue(String attributeName, String value) {
+        Utils.waitForAjaxCallFinish();
+        Selenide.sleep(1000);
+        String nameAttrValue = "contentArea:staticForm:" + attributeName + ":input";
+        ElementsCollection collection = $$x(".//input[@name='" + nameAttrValue + "']");
+        collection.asFixedIterable().forEach((inputField) -> setAttributeValue(attributeName, value, inputField));
     }
 
     private void setAttributeValue(String attributeName, String value) {
@@ -51,19 +76,23 @@ public class SelfRegistrationPage extends LoginPage {
         Selenide.sleep(1000);
         String nameAttrValue = "contentArea:staticForm:" + attributeName + ":input";
         SelenideElement inputField = $x(".//input[@name='" + nameAttrValue + "']");
+        setAttributeValue(attributeName, value, inputField);
+    }
+
+    private void setAttributeValue(String attributeName, String value, SelenideElement inputField) {
+        JavascriptExecutor js = (JavascriptExecutor) WebDriverRunner.getWebDriver();
         try {
-            JavascriptExecutor js = (JavascriptExecutor) WebDriverRunner.getWebDriver();
             js.executeScript("window.stop();");
             inputField
                     .shouldBe(Condition.visible, MidPoint.TIMEOUT_MEDIUM_6_S)
                     .sendKeys(value);
             js.executeScript("window.stop();");
             Selenide.screenshot("try_setAttributeValue_" + attributeName);
+        } catch (ElementShould e) {
+            if (!inputField.is(Condition.visible)) {
+                js.executeScript("document.getElementById(\"" + inputField.getAttribute("id") + "\").value = \"" + value + "\"");
+            }
         } catch (Exception e) {
-//            refresh();
-//            $x(".//input[@name='" + nameAttrValue + "']")
-//                    .shouldBe(Condition.visible, MidPoint.TIMEOUT_MEDIUM_6_S)
-//                    .sendKeys(value);
             Selenide.screenshot("catch_setAttributeValue_" + attributeName);
         }
         Selenide.sleep(1000);
@@ -71,28 +100,54 @@ public class SelfRegistrationPage extends LoginPage {
 
     public SelfRegistrationPage setPassword(String value) {
         Utils.waitForAjaxCallFinish();
+
+        BiConsumer<String, String> processPassword =
+                (elementId, password) ->
+                        $(Schrodinger.byDataId(elementId)).shouldBe(Condition.visible, MidPoint.TIMEOUT_DEFAULT_2_S).sendKeys(password);
+
+        setPassword("password1", value, processPassword);
+        setPassword("password2", value, processPassword);
+
+        return this;
+    }
+
+    private void setPassword(String elementId, String value, BiConsumer<String, String> processPassword) {
         JavascriptExecutor js = (JavascriptExecutor) WebDriverRunner.getWebDriver();
         try {
-            $(Schrodinger.byDataId("password1")).shouldBe(Condition.visible, MidPoint.TIMEOUT_DEFAULT_2_S).sendKeys(value);
+            processPassword.accept(elementId, value);
             js.executeScript("window.stop();");
-            Selenide.screenshot("try_setPassword1_" + value);
+            Selenide.screenshot("try_set" + StringUtils.capitalize(elementId) + "_" + value);
         } catch (Exception e) {
-            Selenide.screenshot("catch_set_password1");
+            Selenide.screenshot("catch_set_" + elementId);
         }
-//        $(Schrodinger.byDataId("password1")).shouldHave(Condition.value(value), MidPoint.TIMEOUT_DEFAULT_2_S);
-        try {
-            $(Schrodinger.byDataId("password2")).shouldBe(Condition.visible, MidPoint.TIMEOUT_DEFAULT_2_S).sendKeys(value);
-            js.executeScript("window.stop();");
-            Selenide.screenshot("try_setPassword2_" + value);
-        } catch (Exception e) {
-            Selenide.screenshot("catch_set_password2");
-        }
-//        $(Schrodinger.byDataId("password2")).shouldHave(Condition.value(value), MidPoint.TIMEOUT_DEFAULT_2_S);
-        js.executeScript("document.querySelectorAll('div[data-s-id=\"validationPanel\"]').item(0).style.display = \"none\";");
-        return  this;
+    }
+
+    public SelfRegistrationPage setAllPasswords(String value) {
+        Utils.waitForAjaxCallFinish();
+
+        BiConsumer<String, String> processPassword = (elementId, password) -> $$(Schrodinger.byDataId(elementId)).asFixedIterable()
+                .forEach((inputField) -> {
+                    try {
+                        inputField.shouldBe(Condition.visible, MidPoint.TIMEOUT_DEFAULT_2_S).sendKeys(password);
+                    } catch (ElementShould e) {
+                        if (!inputField.is(Condition.visible)) {
+                            JavascriptExecutor js = (JavascriptExecutor) WebDriverRunner.getWebDriver();
+                            js.executeScript("document.getElementById(\"" + inputField.getAttribute("id") + "\").value = \"" + value + "\"");
+                        }
+                    }
+                });
+
+        setPassword("password1", value, processPassword);
+        setPassword("password2", value, processPassword);
+
+        return this;
     }
 
     public SelfRegistrationPage submit() {
+        if ($(Schrodinger.byDataId("div", "validationPanel")).is(Condition.visible)) {
+            JavascriptExecutor js = (JavascriptExecutor) WebDriverRunner.getWebDriver();
+            js.executeScript("document.querySelectorAll('div[data-s-id=\"validationPanel\"]').item(0).style.display = \"none\";");
+        }
         Selenide.screenshot("self_reg_before_submit");
         $(Schrodinger.byDataId("submitRegistration")).click();
         Selenide.screenshot("self_reg_after_submit");
