3f191524b5633fd981672b3b40240e318232640a
honchar <honchar@evolveum.com>
2026-01-14 11:08:25 +0100

    added possibility to update xml text while raw object editing

--- schrodinger-framework/src/main/java/com/evolveum/midpoint/schrodinger/page/configuration/RepositoryObjectPage.java
+++ schrodinger-framework/src/main/java/com/evolveum/midpoint/schrodinger/page/configuration/RepositoryObjectPage.java
@@ -16,12 +16,16 @@
 package com.evolveum.midpoint.schrodinger.page.configuration;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
 import com.evolveum.midpoint.schrodinger.MidPoint;
 import com.evolveum.midpoint.schrodinger.page.BasicPage;
 import com.evolveum.midpoint.schrodinger.util.Schrodinger;
+import com.evolveum.midpoint.schrodinger.util.Utils;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$x;
 
 public class RepositoryObjectPage extends BasicPage {
 
@@ -31,6 +35,53 @@ public class RepositoryObjectPage extends BasicPage {
         $(Schrodinger.byDataId("plainTextarea")).shouldBe(Condition.visible, MidPoint.TIMEOUT_MEDIUM_6_S);
         String xml = $(Schrodinger.byDataId("plainTextarea")).getValue();
         return xml == null ? "" : xml;
+    }
+
+    public RepositoryObjectPage insertIntoXmlText(String textToInsert, String textToInsertAfter) {
+        getOptionsCheckBoxByTitleKey("pageDebugView.switchToPlainText.help").setSelected(true)
+                .shouldBe(Condition.selected, MidPoint.TIMEOUT_SHORT_4_S);
+        SelenideElement textArea = $(Schrodinger.byDataId("plainTextarea"))
+                .shouldBe(Condition.visible, MidPoint.TIMEOUT_MEDIUM_6_S);
+        String xml = textArea.getValue();
+        if (xml == null) {
+            xml = "";
+        }
+        if (xml.contains(textToInsertAfter)) {
+            int position = xml.indexOf(textToInsertAfter) + textToInsertAfter.length();
+            xml = xml.substring(0, position) + textToInsert + xml.substring(position + 1);
+        } else {
+            xml = xml + textToInsert;
+        }
+        textArea.setValue(xml);
+        Selenide.sleep(1000);
+        return RepositoryObjectPage.this;
+    }
+
+    private SelenideElement getOptionsCheckBoxByTitleKey(String checkBoxTitleKey) {
+        return $x(".//div[@title='" + checkBoxTitleKey + "']")
+                .shouldBe(Condition.visible, MidPoint.TIMEOUT_DEFAULT_2_S)
+                .$x(".//input[@type='checkbox']")
+                .shouldBe(Condition.visible, MidPoint.TIMEOUT_DEFAULT_2_S);
+    }
+
+    public ListRepositoryObjectsPage clickSaveButton() {
+        SelenideElement buttonsPanel = $x(".//fieldset[contains(@class, 'objectButtons')]")
+                .shouldBe(Condition.visible, MidPoint.TIMEOUT_DEFAULT_2_S);
+        buttonsPanel.$x(".//a[contains(text(), 'Save')]")
+                .shouldBe(Condition.visible, MidPoint.TIMEOUT_DEFAULT_2_S)
+                .click();
+        Utils.waitForAjaxCallFinish();
+        return new ListRepositoryObjectsPage();
+    }
+
+    public ListRepositoryObjectsPage clickBackButton() {
+        SelenideElement buttonsPanel = $x(".//fieldset[contains(@class, 'objectButtons')]")
+                .shouldBe(Condition.visible, MidPoint.TIMEOUT_DEFAULT_2_S);
+        buttonsPanel.$x(".//a[contains(text(), 'Back')]")
+                .shouldBe(Condition.visible, MidPoint.TIMEOUT_DEFAULT_2_S)
+                .click();
+        Utils.waitForAjaxCallFinish();
+        return new ListRepositoryObjectsPage();
     }
 
     public RepositoryObjectPage assertObjectXmlContainsText(String expectedText) {
