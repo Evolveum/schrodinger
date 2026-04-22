e36f97c3f229cebcca073c068549d405dffd831a
honchar <honchar@evolveum.com>
2026-04-22 13:30:36 +0200

    Fix for ObjectListArchetypeTests

--- schrodinger-framework/src/main/java/com/evolveum/midpoint/schrodinger/component/assignmentholder/AssignmentHolderObjectListTable.java
+++ schrodinger-framework/src/main/java/com/evolveum/midpoint/schrodinger/component/assignmentholder/AssignmentHolderObjectListTable.java
@@ -161,8 +161,8 @@ public abstract class AssignmentHolderObjectListTable<P, PD extends AssignmentHo
             ElementsCollection childrenButtonCollection = modal.$$x(".//div[@data-s-id='additionalButton']");
             int count = childrenButtonCollection.size();
             modal.$x(".//a[@data-s-id='cancelButton']").click();
-            Selenide.sleep(1000);
             Utils.waitForAjaxCallFinish();
+            Selenide.sleep(1500);
             modal.shouldBe(Condition.disappear, MidPoint.TIMEOUT_LONG_20_S);
             return count;
         }
