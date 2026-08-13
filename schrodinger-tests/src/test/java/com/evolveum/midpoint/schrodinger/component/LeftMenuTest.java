b7b52b1dde986e91a212a540e1032576c4e32cd6
honchar <honchar@evolveum.com>
2026-08-13 10:11:38 +0200

    Disabled the test
    Task: 11041

--- schrodinger-tests/src/test/java/com/evolveum/midpoint/schrodinger/component/LeftMenuTest.java
+++ schrodinger-tests/src/test/java/com/evolveum/midpoint/schrodinger/component/LeftMenuTest.java
@@ -61,7 +61,7 @@ public class LeftMenuTest extends AbstractSchrodingerTest {
     }
 
     //covers #10998
-    @Test
+    @Test (enabled = false)
     public void test00200testObjectCollectionViewMenuTranslated() {
         reloginAsAdministrator();
         importObject(SYSTEM_CONFIG_WITH_OBJ_COLLECTION, true);
